bbdd7640aa044b663ab002c897d1c6ebd96e3b2b
Mineez <mm0625257659@gmail.com>
2022-01-16 12:14:15 +0700

    00

---
+++ lib/Pig.dart
@@ -0,0 +1,28 @@
+class Pig {
+  var _length;
+  var _girth;
+  Pig(var length, var girth) {
+    this._length = length*0.01;
+    this._girth = girth*0.01;
+  }
+
+  double cal() {
+    return _girth*_girth*_length*69.3;
+  }
+
+  double calPlus() {
+    return ((cal()*3)/100) + cal();
+  }
+
+  double calMinus() {
+    return cal() - ((cal()*3)/100);
+  }
+
+  double PPrice() {
+    return calPlus()*112.50;
+  }
+
+  double MPrice() {
+    return calMinus()*112.50;
+  }
+}
